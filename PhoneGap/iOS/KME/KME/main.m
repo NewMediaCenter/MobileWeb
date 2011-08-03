8af0b77399e5362f885f0a48ae993575fa8211cb
natjohns <natjohns@c58b5278-a2c8-4678-9377-d879b98ccb7f>
2011-08-03 20:24:53 +0000

    MOBILITY-45

---
+++ PhoneGap/iOS/KME/KME/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  KME
+//
+//  Created by Nathaniel Johnson on 8/3/11.
+//  Copyright Indiana University 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
